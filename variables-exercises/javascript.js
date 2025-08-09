4987a43a4ceac2e9189f0337c50bb08c7243f4de
Eytan Khoutorsky <khoutorskye@gmail.com>
2025-08-08 21:17:02 -0400

    just some exercises on js variables

---
+++ variables-exercises/javascript.js
@@ -0,0 +1,33 @@
+console.log((3+2)-76*(1+1));
+
+console.log(23 + 97);
+
+console.log(23 + 97+3+234+23+69);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+
+console.log(a);
+
+a = 15;
+console.log(a);
+
+let b=7*a;
+
+console.log(b);
+
+const max = 57;
+
+const actual = max-13;
+
+const percentage = actual/max;
+
+console.log(percentage);
+alert(percentage);
+
+
+const myInt = 5;
+const myFloat = 6.667;
+typeof myInt;
+typeof myFloat;
